cce99a6dc8680e30b8d3d65140c33d003132d622
Twotronix <twotronix@gmail.com>
2026-09-08 18:44:50 +0100

    Slightly Optimised Code

--- app.js
+++ app.js
@@ -13,13 +13,11 @@ const lastTheme = themes.length
 document.body.classList.toggle(themes[indx])
 
 function nextTheme() {
-    document.body.classList.toggle(themes[indx])
     indx += 1
     if (indx == lastTheme) {
         indx = 0
     }
-    document.body.className = themes[indx];
-    
+    document.body.className = themes[indx];    
 }
 Btn.addEventListener("click", () => {
     nextTheme()
